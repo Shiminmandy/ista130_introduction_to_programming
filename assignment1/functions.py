cbfa5deb05e035852638cb11e40251ef35fa93f9
Shiminmandy <mandyshimin18@gmail.com>
2020-06-08 23:52:34 -0400

    add sum3 in assignment1

--- assignment1/functions.py
+++ assignment1/functions.py
@@ -7,9 +7,15 @@ def chases(predator, prey_animal):
     # print("the{}chases the{}".format(predator, prey_animal))
     print(f"The {predator} chases the {prey_animal}")
 
+def sum3(var1, var2, var3):
+    print(f"The sum of the arguments is: {var1 + var2 + var3}")
+
+
 def main():
     chases('dragon', 'human')
     chases('coyote', 'roadrunner')
+    sum3(1, 2, 3)
+    sum3(4, 5, 6)
 if __name__ == '__main__':  # 只会在本文件进行以下方法调用
     main()
 
