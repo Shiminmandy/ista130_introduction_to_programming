992ba14a366546b69ceb81cc058457498914208e
Shiminmandy <mandyshimin18@gmail.com>
2020-06-09 14:26:18 -0400

    add first function in assignment2

--- assignment2/functions2.py
+++ assignment2/functions2.py
@@ -3,3 +3,16 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+
+def print_word(int, str):
+    for i in range(int):
+        print(f"{i + 1} --> {str}")
+
+
+def main():
+    print_word(3, 'banana')
+    print_word(4, 'mississippi')
+
+
+if __name__ == '__main__':
+    main()
---
+++ assignment2/sdfghj.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Description
+# @Author: Shimin
+# @Copyright
+# @Version:0.0.1
