70e56749619547c93a1ab85a58d2f246a22d9c0c
Shiminmandy <mandyshimin18@gmail.com>
2020-06-09 16:17:56 -0400

    remain last one

--- assignment2/functions2.py
+++ assignment2/functions2.py
@@ -3,6 +3,8 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+import math
+
 
 def print_word(int, str):
     for i in range(int):
@@ -16,7 +18,8 @@ def bacteria(num_mints, num_bacteria):
 
 def convert_to_copper(num_gold_coins, num_silver_coins, num_copper_coins):
     converted_cp = num_gold_coins * 50 + num_silver_coins * 5 + num_copper_coins
-    print(f"{num_gold_coins } gp, {num_silver_coins} sp, {num_copper_coins} cp converted to copper is: {converted_cp} cp")
+    print(
+        f"{num_gold_coins} gp, {num_silver_coins} sp, {num_copper_coins} cp converted to copper is: {converted_cp} cp")
 
 
 def convert_from_copper(num_cp):
@@ -26,7 +29,28 @@ def convert_from_copper(num_cp):
     print(f"{num_cp} copper pieces is: {num_gp} gp, {num_sp} sp, {rest_cp} cp")
 
 
+def repeat_word(a_string, num_rows, num_col):
+    i = 0
+    while i < num_rows:
+        print(a_string * num_col)
+        i += 1
 
+
+def text_triangle(int6):
+    for i in range(int6):
+        print('x' * (i + 1))
+
+
+def surface_area_of_cylinder(radius, height):
+    SA = 2 * math.pi * radius ** 2 + 2 * math.pi * radius * height
+    print(f"The surface area of a cylinder with radius {radius} and height {height} is {SA}")
+
+
+def tree_height(distance, length):
+    Height = math.sqrt(length ** 2 - distance ** 2)
+    print(f"Kite string: {length}")
+    print(f"Distance: {distance}")
+    print(f"Height: {Height}")
 
 
 def main():
@@ -39,6 +63,17 @@ def main():
     convert_from_copper(200)
     convert_from_copper(1107)
     convert_from_copper(3242)
+    repeat_word('Goblin', 3, 5)
+    repeat_word('Kobold', 5, 3)
+    text_triangle(3)
+    text_triangle(10)
+    text_triangle(0)
+    surface_area_of_cylinder(10.0, 10.0)
+    surface_area_of_cylinder(0.0, 1.0)
+    tree_height(300, 500)
+    tree_height(100, 141.421356)
+
+
 
 if __name__ == '__main__':
     main()
--- assignment2/sdfghj.py
+++ assignment2/sdfghj.py
@@ -15,4 +15,18 @@ for i in range(5):
 print(200 // 50)
 print((1107 % 50) // 5)
 print(3242 % 50 // 5)
+print('hobbit' * 2)
+i = 1
+while i < 3:
+    print('Dog' * 5)
+    i += 1
 
+
+i = 0
+while i <= 4:
+    print('X' * (i))
+    i += 1
+
+print('X''\n''XX')
+for i in range(6):
+    print(f"{'A' * i}")
