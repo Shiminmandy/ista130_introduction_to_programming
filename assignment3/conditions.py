0a85d72e4389dce82ad686b1a1320239d6fa5a5d
Shiminmandy <mandyshimin18@gmail.com>
2020-06-10 01:12:59 -0400

    finished assignment3

--- assignment3/conditions.py
+++ assignment3/conditions.py
@@ -47,6 +47,72 @@ def pants_size(size_number):
         return 'medium'
     else:
         return 'small'
+
+
+def pants_fitter():
+    name = input("Enter your name:")
+    print(f"Greetings {name} welcome to Pants-R-Us")
+    waist_size = input("Enter your waist size in inches:")
+    num_pants = input("How many pairs of pants would you like:")
+    regular_or_fancy = input("Would you like regular or fancy pants?")
+    if regular_or_fancy == 'fancy':
+        cost = 100 * num_pants
+    else:
+        cost = 40 * num_pants
+    print(f"{num_pants} pairs of {pants_size(waist_size)} {regular_or_fancy} pants: $ {cost}")
+
+
+def digdug(int7):
+    for i in range(1, int7+1):
+        if i % 3 == 0 and i % 15 != 0:
+            print(f"{i} : dig")
+        elif i % 5 == 0 and i % 15 != 0:
+            print(f"{i} : dug")
+        elif i % 15 == 0:
+            print(f"{i} : digdug")
+            i += 1
+
+
+def beef_type(percent_lean):
+    if percent_lean < 78:
+        return "Hamburger"
+    elif percent_lean >= 78 and percent_lean < 85:
+        return "Chuck"
+    elif percent_lean >= 85 and percent_lean < 90:
+        return "Round"
+    elif percent_lean >= 90 and percent_lean <= 95:
+        return "Sirloin"
+    else:
+        return "Unknown"
+
+
+def species_height(H_or_K, height):
+    if H_or_K == "Human" and height > 67:
+        print("Above Average")
+    elif H_or_K == "Human" and height < 67:
+        print("Below Average")
+    elif H_or_K == "Klingon" and height > 71:
+        print("Above Average")
+    elif H_or_K == "Klingon" and height < 71:
+        print("Below Average")
+    elif (H_or_K == "Klingon" and height == 71):
+        print("Average")
+    elif (H_or_K == "Human" and height == 67):
+        print("Average")
+
+
+def sooner_date(var1, var2, var3, var4):
+    if var1 > var3:
+        print(f"{var3} / {var4}")
+    elif var1 < var3:
+        print(f"{var1} / {var2}")
+    elif var1 == var3 and var2 > var4:
+        print(f"{var3} / {var4}")
+    elif var1 == var3 and var2 < var4:
+        print(f"{var1} / {var2}")
+    elif var1 == var3 and var2 == var4:
+        print(f"{var1} / {var2}")
+
 def main():
     word_length('liversnaps', 7)
     word_length('earwax', 5)
@@ -65,6 +131,17 @@ def main():
     is_normal_blood_pressure(119, 79)
     is_normal_blood_pressure(120, 79)
     pants_size(38)
-
+    digdug(5)
+    digdug(2)
+    digdug(3)
+    digdug(5)
+    digdug(15)
+    beef_type(91.2)
+    beef_type(78)
+    beef_type(87)
+    beef_type(95.1)
+    sooner_date(1, 1, 1, 2)
+    sooner_date(2, 5, 1, 3)
+    sooner_date(8, 25, 7, 30)
 if __name__ == '__main__':
     main()
