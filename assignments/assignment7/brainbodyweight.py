5774964bc63f269d9d41af2058090aae002e8530
Shiminmandy <mandyshimin18@gmail.com>
2020-06-15 00:19:53 -0400

    25%

--- assignments/assignment7/brainbodyweight.py
+++ assignments/assignment7/brainbodyweight.py
@@ -3,11 +3,33 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+"""
+1. 如果列表为空，
+    1.1 返还0
+2. 如果列表不包含动物名
+    2.1把名字加入列表list.insert()
+    2.2将列表中的名字按顺序排列list.sort()
+    2.3list.index()找到动物名位置，储存于变量中
+    2.4list.pop(index)去掉加入的动物名
+    2.5列表只返还加入动物名的位置
+3. 如果列表包含动物名
+    3.1直接返回动物名的位置
+
+"""
 
 def find_insert_position(name, string_order):
-    for i in string_order:
-        string_order.insert(i, name)
-        print(string_order.index(name))
+    if string_order == []:
+
+        return 0
+    elif name not in string_order:
+        string_order.insert(0, name)
+        string_order.sort()
+        location = string_order.index(name)
+        string_order.pop(location)
+        return location
+
+    elif name in string_order:
+        return string_order.index(name)
 def main():
     find_insert_position('apple', ['banana', 'orange'])
 if __name__ == '__main__':
--- assignments/assignment7/draft7.py
+++ assignments/assignment7/draft7.py
@@ -3,3 +3,7 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+lst = []
+#for i in range(len(lst)):
+#for i in lst:
+print(lst.index(1))
