7e7619237c4824554579806a46d0b83df1c0ac43
Shiminmandy <mandyshimin18@gmail.com>
2020-06-14 23:02:35 -0400

    add draft

--- assignments/assignment7/brainbodyweight.py
+++ assignments/assignment7/brainbodyweight.py
@@ -3,3 +3,13 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+
+def find_insert_position(name, string_order):
+    for i in string_order:
+        string_order.insert(i, name)
+        print(string_order.index(name))
+def main():
+    find_insert_position('apple', ['banana', 'orange'])
+if __name__ == '__main__':
+    main()
+
---
+++ assignments/assignment7/draft7.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Description:draft for assignment7
+# @Author: Shimin
+# @Copyright
+# @Version:0.0.1
