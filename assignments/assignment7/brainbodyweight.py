4c980a304850754b7753b6276504c06028cdc543
Shiminmandy <mandyshimin18@gmail.com>
2020-06-16 01:27:23 -0400

    75%

--- assignments/assignment7/brainbodyweight.py
+++ assignments/assignment7/brainbodyweight.py
@@ -43,34 +43,66 @@ def find_insert_position(name, string_order):
 """
 def populate_lists(names, body_weights, brain_weights):
 
-    #csv_file = open('BrainBodyWeightKilos.csv', 'r')
-    with open('BrainBodyWeightKilos.csv') as csv_file:
-        readCSV = csv.reader(csv_file, delimiter=',')
-        for line in readCSV:
-            names.append(line[0].title())
-            body_weights.append(line[1])
-            brain_weights.append(line[2])
-    #print(csv_file)
-    """
+    csv_file = open('BrainBodyWeightKilos.csv', 'r')
     for line in csv_file:
         splited = line[0:-1].split(',')
-        print(splited)
+        #print(splited)
         names.append(splited[0].title())
-        body_weights.append(splited[1])
-        brain_weights.append(splited[2])
+        body_weights.append(float(splited[1]))
+        brain_weights.append(float(splited[2]))
 
-    csv_file.close()"""
+    csv_file.close()
     #return names, body_weights, brain_weights we do not return list
 
+    # with open('BrainBodyWeightKilos.csv') as csv_file:
+    #     readCSV = csv.reader(csv_file, delimiter=',')
+    #     for line in readCSV:
+    #         names.append(line[0].title())
+    #         body_weights.append(line[1])
+    #         brain_weights.append(line[2])
+    # #print(csv_file)
 
 
+"""
+1.体重列表中的数据*2.205
+2.脑重列表中的数据*0.0022
+3.创建并打开新文件
+4.因为每个列表的length相同，所以直接循环一个列表
+    4.2 write()加入三个列表中同一个index的三个值，改成指定模式
+5. 关上文件
+"""
+def write_converted_csv(filename, lst_names,BodyWeights ,BrainWeights):
+    lst_body = []
+    for i in BodyWeights:
+        lst_body.append(round(i * 2.205, 2))
+    lst_brain = []
+    for i in BrainWeights:
+        lst_brain.append(round(i*0.0022, 2))
+    fname = filename
+    new_file = open(fname, 'w')
+    for i in range(len(lst_names)):
+        new_file.write(f"{lst_names[i].title()},")
+        new_file.write(f"{lst_body[i]},")
+        new_file.write(f"{lst_brain[i]}\n")
+    new_file.close()
 
-# def write_converted_csv(filename, lst_names, lst_body, lst_brain):
+    # writeCSV = csv.writer(new_file, delimiter=',')
+    # writeCSV.writerows(lst_names)
+    # writeCSV.writerows(lst_body)
+    # writeCSV.writerows(lst_brain)
+    # new_file.close()
+
+
 
 
 def main():
-    find_insert_position('apple', ['banana', 'orange'])
     populate_lists([],[],[])
+    write_converted_csv('whatever.csv', ['apple', 'banana', 'orange'], [10, 20, 30], [1.2, 2.3, 4.5])
+    # answer = 'q'
+    # while not answer =='q' or answer == 'Q':
+    #     answer = input(f'Enter animal name (or "q" to quit) : ').title()
+
+
 if __name__ == '__main__':
     main()
 
--- assignments/assignment7/draft7.py
+++ assignments/assignment7/draft7.py
@@ -3,7 +3,9 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
-lst = ['apple banana orange']
+lst1 = ['apple', 'banana', 'orange']
+lst2 = [123, 234, 345]
+lst3 = [100,100,100]
 #for i in range(len(lst)):
-#for i in lst:
-print(lst[1])
+new = zip(lst1, lst2, lst3)
+print(new)
