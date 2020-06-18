3c1474b5a5314a081d084e867b62f15de08cb7b7
Shiminmandy <mandyshimin18@gmail.com>
2020-06-18 17:38:11 -0400

    50%

---
+++ assignments/assignment8/emoticons.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Description: assignment8(2)
+# @Author: Shimin
+# @Copyright
+# @Version:0.0.1
--- assignments/assignment8/fishcatch.py
+++ assignments/assignment8/fishcatch.py
@@ -11,14 +11,40 @@ def fish_dict_from_file(filename):
     openfile = open(filename, 'r')
     for line in openfile:
         splited = line.split(" ")
-        #print(splited)
+        # print(splited)
+        # without_list = filter(lambda x: x != "", splited)
+        # print(without_list) # 需要用list接住被过滤的元素
         filtered = list(filter(lambda x: x != "", splited))
-        #print(filter(lambda x: x != "", splited))
-        print(filtered)
-        #the_name = fishmap[filtered[1]]
+        # print(filter(lambda x: x != "", splited))
+        # print(filtered)
+
+
+        the_name = fishmap[filtered[1]] # filtered[1]是key
         #print(the_name)
 
+        if the_name not in fish_dict.keys():
+            if filtered[2] != 'NA':
+                fish_dict[the_name] = [float(filtered[2])]
+        else:
+            if filtered[2] != 'NA':
+                fish_dict[the_name].append(float(filtered[2]))
+    return fish_dict
+    #print(fish_dict)
+
+
+
 def main():
-    fish_dict_from_file('fishcatch.dat')
+    fish_dict = fish_dict_from_file('fishcatch.dat')
+
+    first = '#'
+    second = 'NAME'
+    third = 'MEAN WT'
+    print(f"{first.rjust(4)} {second.ljust(10)}{third.rjust(11)}")
+    for key, val in sorted(fish_dict.items()):  # sort 是应用在 list 上的方法，sorted 可以对所有可迭代的对象进行排序操作
+        print(f'{str(len(val)).rjust(4)} {str(key).ljust(10)}{str(round(sum(val) / len(val), 1)).rjust(10)}g')
+
+
+
+
 if __name__ == '__main__':
     main()
