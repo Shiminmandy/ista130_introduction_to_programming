cec4b705198270540e68331361878224e7cd40d1
Shiminmandy <mandyshimin18@gmail.com>
2020-06-18 21:53:10 -0400

    60%

---
+++ assignments/assignment8/draft.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# @Description
+# @Author: Shimin
+# @Copyright
+# @Version:0.0.1
+
+person = {}
+person[str("Age")]= '18'
+print(person)
--- assignments/assignment8/emoticons.py
+++ assignments/assignment8/emoticons.py
@@ -3,3 +3,32 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+
+
+def load_twitter_dicts_from_file(filename, emoticons_to_ids, ids_to_emoticons):
+    openfile = open(filename, 'r')
+    for line in openfile:
+        splited = line.split(" ")
+        # print(splited[0].strip('"'))
+        # print(splited)
+        # emoticon = splited[0].strip('"')
+        emoticon = splited[0][1:-1]
+        userID = splited[2][1:-1]
+        # print(emoticon)
+        if emoticon not in emoticons_to_ids.keys():
+            emoticons_to_ids[emoticon] = [userID]
+            # print(emoticons_to_ids)
+        else:
+            emoticons_to_ids[emoticon].append(userID)
+    # print(emoticons_to_ids)
+
+        if userID not in ids_to_emoticons.keys():
+            ids_to_emoticons[userID] = [emoticon]
+        else:
+            ids_to_emoticons[userID].append(emoticon)
+    # print(ids_to_emoticons)
+def main():
+    load_twitter_dicts_from_file("twitter_emoticons.dat", {}, {})
+
+if __name__ == '__main__':
+    main()
