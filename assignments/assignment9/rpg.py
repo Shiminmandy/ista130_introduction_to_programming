836b5b52d4cb750e657f3d49392bee838c3cd84b
Shiminmandy <mandyshimin18@gmail.com>
2020-06-23 21:48:51 -0400

    28%

--- assignments/assignment9/rpg.py
+++ assignments/assignment9/rpg.py
@@ -1,5 +1,13 @@
 # -*- coding:utf-8 -*-
-# @Description
+# @Description: pyhton assignment9
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+
+class Fighter:
+    def __init__(self, name):
+        self.name = name
+        self.hit_points = 10
+
+    def __repr__(self):
+        return f'{self.name} (HP: {self.hit_points})'
