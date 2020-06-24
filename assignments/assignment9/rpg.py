d8815b2eb5835389f2899b5bd485bb0515ac0cbd
Shiminmandy <mandyshimin18@gmail.com>
2020-06-23 21:58:27 -0400

    43%

--- assignments/assignment9/rpg.py
+++ assignments/assignment9/rpg.py
@@ -11,3 +11,10 @@ class Fighter:
 
     def __repr__(self):
         return f'{self.name} (HP: {self.hit_points})'
+
+    def take_damage(self, damage_amount):
+        self.hit_points -= damage_amount
+        if self.hit_points <= 0:
+            print(f'\tAlas, {self.name} has fallen!')
+        else:
+            print(f'\t{self.name} has {self.hit_points} hit points remaining.')
